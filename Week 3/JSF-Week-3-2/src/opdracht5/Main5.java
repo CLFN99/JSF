646c2959964ee5df35dfb3f503215b7130552ac1
Jort Geurts <jort@elska.nl>
2017-09-13 05:47:27 -0700

    Finished opdracht 5

--- Week 3/JSF-Week-3-2/src/opdracht5/Main5.java
+++ Week 3/JSF-Week-3-2/src/opdracht5/Main5.java
@@ -4,15 +4,40 @@ import java.io.*;
 
 public class Main5 {
     public static void main(String[] args) throws IOException {
+        System.out.println("Ayy");
         if (args.length != 1) {
             System.err.println("Usage: java OSProcess <command>");
             System.exit(0);
         }
 
+        //===ProcessBuilder way===
+
         // args[0] is the command
         ProcessBuilder pb = new ProcessBuilder(args[0]);
         Process proc = pb.start();
 
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        proc.destroy();
+
+        //===Exec way===
+
+        try {
+            Process p = Runtime.getRuntime().exec(args[0]);
+            Thread.sleep(1000);
+            p.destroy();
+            int exitVal = p.waitFor();
+            System.out.println(exitVal);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        //Other random code from the sheet that does things
         // Obtain the input stream
         InputStream is = proc.getInputStream();
         InputStreamReader isr = new InputStreamReader(is);
@@ -25,5 +50,6 @@ public class Main5 {
         }
 
         br.close();
+        */
     }
 }
