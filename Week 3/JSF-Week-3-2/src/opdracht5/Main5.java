78061abd385c1bcf2c7c3a4fb34af13393fa17a8
Jort Geurts <jort@elska.nl>
2017-09-14 11:03:24 +0200

    Finished opdracht9 and opdracht11

--- Week 3/JSF-Week-3-2/src/opdracht5/Main5.java
+++ Week 3/JSF-Week-3-2/src/opdracht5/Main5.java
@@ -1,9 +1,14 @@
 package opdracht5;
 
+import timeutil.TimeStamp;
+
 import java.io.*;
 
+@SuppressWarnings("Duplicates")
 public class Main5 {
     public static void main(String[] args) throws IOException {
+        TimeStamp ts = new TimeStamp();
+
         System.out.println("Ayy");
         if (args.length != 1) {
             System.err.println("Usage: java OSProcess <command>");
@@ -12,35 +17,45 @@ public class Main5 {
 
         //===ProcessBuilder way===
 
-        /*
+        ts.setBegin("ProcessBuilder");
         // args[0] is the command
         ProcessBuilder pb = new ProcessBuilder(args[0]);
+        ts.setEnd("pb instance created");
         Process proc = pb.start();
+        ts.setEnd("pb process started");
 
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        ts.setEnd("pb process lived for 1000ms");
         proc.destroy();
-        */
+        ts.setEnd("Done ProcessBuilder");
 
         //===Exec way===
 
         try {
+            ts.setEndBegin("Beginning Runtime.exec()");
+            // Execute the command
             Process p = Runtime.getRuntime().exec(args[0]);
+            ts.setEnd("process started");
             Thread.sleep(1000);
+            ts.setEnd("process lived for 1000ms");
+
+
             /*
-            p.destroy();
             int exitVal = p.waitFor();
             System.out.println(exitVal);
             */
 
             // Obtain the input stream
             InputStream is = p.getInputStream();
+            ts.setEnd("InputStream created");
             InputStreamReader isr = new InputStreamReader(is);
+            ts.setEnd("InputStreamReader created");
             BufferedReader br = new BufferedReader(isr);
+            ts.setEnd("BufferedReader created");
 
             // Read what is returned by the command
             String line;
@@ -48,7 +63,17 @@ public class Main5 {
                 System.out.println(line);
             }
 
+            ts.setEnd("Spammed string variables");
+
             br.close();
+
+            ts.setEnd("Closed reader");
+
+            p.destroy();
+
+            ts.setEnd("Destroyed process");
+
+            System.out.print(ts.toString());
 
         } catch (InterruptedException e) {
             e.printStackTrace();
--- Week 3/JSF-Week-3-2/src/opdracht9/runnable/ProcessBuilderRunnable.java
+++ Week 3/JSF-Week-3-2/src/opdracht9/runnable/ProcessBuilderRunnable.java
@@ -1,4 +1,45 @@
 package opdracht9.runnable;
 
-public class ProcessBuilderRunnable {
+import timeutil.TimeStamp;
+
+import java.io.IOException;
+
+public class ProcessBuilderRunnable implements Runnable {
+
+    private TimeStamp ts;
+    private String[] args;
+
+    public ProcessBuilderRunnable(String[] args)  {
+        ts = new TimeStamp();
+
+        this.args = args;
+    }
+
+    @Override
+    public void run() {
+        //===ProcessBuilder way===
+
+        try {
+            ts.setBegin("ProcessBuilder");
+            // args[0] is the command
+            ProcessBuilder pb = new ProcessBuilder(args);
+            ts.setEnd("pb instance created");
+            Process proc = pb.start();
+            ts.setEnd("pb process started");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ts.setEnd("pb process lived for 1000ms");
+            proc.destroy();
+            ts.setEnd("Done ProcessBuilder");
+
+            System.out.print(ts.toString());
+            System.out.println("Process " + args[0] + " with arg " + args[1] + " is terminated");
+        } catch(IOException io) {
+            io.printStackTrace();
+        }
+    }
 }
