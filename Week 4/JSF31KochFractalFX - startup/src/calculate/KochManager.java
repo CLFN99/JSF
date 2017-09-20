4eafd15bde9eceabefbfddbb495b08d10a706e90
Célina Ngapy <c.ngapy@student.fontys.nl>
2017-09-20 11:45:53 +0200

    4+5 done

--- Week 4/JSF31KochFractalFX - startup/src/calculate/KochManager.java
+++ Week 4/JSF31KochFractalFX - startup/src/calculate/KochManager.java
@@ -1,7 +1,10 @@
 package calculate;
 
 import jsf31kochfractalfx.JSF31KochFractalFX;
+import timeutil.TimeStamp;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -9,16 +12,23 @@ public class KochManager implements Observer {
 
     private JSF31KochFractalFX application;
     private KochFractal koch;
+    private ArrayList<Edge> edges;
+    private TimeStamp time;
 
     public KochManager(JSF31KochFractalFX application) {
         this.application = application;
         koch = new KochFractal();
         koch.addObserver(this);
+        edges = new ArrayList<>();
+        time = new TimeStamp();
     }
 
     @Override
     public void update (Observable o, Object arg) {
-        application.drawEdge((Edge)arg);
+        edges.add((Edge)arg);
+        for (Edge e:edges) {
+            application.drawEdge((Edge) arg);
+        }
     }
 
 
@@ -27,10 +37,16 @@ public class KochManager implements Observer {
         drawEdges();
     }
     public void drawEdges() {
+        time.setBegin("Edges are being drawn..");
         application.clearKochPanel();
+
         koch.generateLeftEdge();
         koch.generateBottomEdge();
         koch.generateRightEdge();
+
+        time.setEnd("Fractal done!");
+        application.setTextCalc(time.toString());
+
     }
 
 }
