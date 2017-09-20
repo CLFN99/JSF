2486b633e2027532c33cb127d809252dfb6afe4e
MagicLegend <jort@elska.nl>
2017-09-20 14:27:23 +0200

    Updated kochmanager up to opdracht10

--- Week 4/JSF31KochFractalFX - startup/src/calculate/KochManager.java
+++ Week 4/JSF31KochFractalFX - startup/src/calculate/KochManager.java
@@ -3,51 +3,60 @@ package calculate;
 import jsf31kochfractalfx.JSF31KochFractalFX;
 import timeutil.TimeStamp;
 
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 
 public class KochManager implements Observer {
 
     private JSF31KochFractalFX application;
     private KochFractal koch;
-    private ArrayList<Edge> edges;
+    private List<Edge> edges;
     private TimeStamp time;
+    private ArrayList<String> calcTimes;
 
     public KochManager(JSF31KochFractalFX application) {
         this.application = application;
         koch = new KochFractal();
         koch.addObserver(this);
-        edges = new ArrayList<>();
+        edges = new LinkedList<>();
         time = new TimeStamp();
+        calcTimes = new ArrayList<>();
     }
 
     @Override
-    public void update (Observable o, Object arg) {
+    public void update(Observable o, Object arg) {
         edges.add((Edge)arg);
-        for (Edge e:edges) {
-            application.drawEdge((Edge) arg);
-        }
         application.setTextNrEdges(String.valueOf(koch.getNrOfEdges()));
     }
 
 
     public void changeLevel(int nxt) {
         koch.setLevel(nxt);
-        drawEdges();
-    }
-    public void drawEdges() {
-        time.setBegin("Edges are being drawn..");
-        application.clearKochPanel();
+
+        edges.clear();
+
+        time.setBegin("Edges are being generated..");
 
         koch.generateLeftEdge();
         koch.generateBottomEdge();
         koch.generateRightEdge();
 
-        time.setEnd("Fractal done!");
-        application.setTextCalc(time.toString());
+        time.setEnd("Fractal generation done!");
 
+        drawEdges();
+    }
+    public void drawEdges() {
+
+        application.clearKochPanel();
+
+        time.setBegin("Edges are being drawn..");
+        for (Edge e:edges) {
+            application.drawEdge(e);
+        }
+        time.setEnd("Fractal drawing done!");
+
+        //application.setTextCalc(time.toString());
+        calcTimes.add(time.toString()); //Add the string to the calcTimes array
+        System.out.println(calcTimes.get(calcTimes.size() - 1)); //Log the full array
     }
 
 }
