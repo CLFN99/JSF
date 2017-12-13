75f0d03de5da4e0f98995cbfbdf92586b516403c
MagicLegend <jort@elska.nl>
2017-12-13 09:28:08 +0100

    Added new week 14 project

---
+++ Week 14/Week_14/src/com/magiclegend/Writing/TextWithBuffer.java
@@ -0,0 +1,68 @@
+package com.magiclegend.Writing;
+
+import calculate.Edge;
+import calculate.KochFractal;
+import com.google.gson.Gson;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+public class TextWithBuffer extends Application implements Observer {
+    private static List<Edge> edges = new ArrayList<Edge>();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        edges.add((Edge)arg);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        KochFractal fractal = new KochFractal();
+        fractal.addObserver(this);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int level = 0;
+        System.out.print("Enter level:");
+        try{
+            level = Integer.parseInt(br.readLine());
+
+        }catch(NumberFormatException nfe){
+            System.err.println("Invalid Format!");
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //generate edges
+        if(level != 0) {
+            fractal.setLevel(level);
+            fractal.generateBottomEdge();
+            fractal.generateLeftEdge();
+            fractal.generateRightEdge();
+
+            try {
+                if (edges.size() == fractal.getNrOfEdges()) {
+                    FileWriter writer = new FileWriter(level + ".json");
+                    BufferedWriter out = new BufferedWriter(writer);
+                    Gson gson = new Gson();
+                    String jsonString = gson.toJson(edges);
+                    writer.write(jsonString);
+
+                    out.flush();
+                    out.close();
+                    System.out.println("done!");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
